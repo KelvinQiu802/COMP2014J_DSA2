3228347411a31d19c9bdc348246fe66f3cd4bb19
KelvinQiu <kelvinqiu802@outlook.com>
2023-03-07 11:55:37 +0800

    feat: lab01 init

---
+++ WS1-Trees/src/Q1.java
@@ -0,0 +1,10 @@
+import dsa.iface.ITree;
+import dsa.impl.Tree;
+
+public class Q1 {
+   public static void main( String[] args ) {
+      ITree<Character> tree = Tree.createTreeA();
+      
+      // write your code to answer Question 1 here...
+   }
+}
